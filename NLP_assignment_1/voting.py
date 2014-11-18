bc7f743fd847b66697c2e2521ab1b514833abbf9
Michael A <xmxmichael@gmail.com>
2014-11-19 00:14:29 +0200

    voting skeleton fixed

--- NLP_assignment_1/voting.py
+++ NLP_assignment_1/voting.py
@@ -3,7 +3,7 @@ from nltk import TaggerI, SequentialBackoffTagger
 __author__ = 'michael'
 
 
-class EntropyTaggerI(TaggerI):
+class EntropyTaggerI(SequentialBackoffTagger):
 
     def possible_tags(self, word):
         return {'VERB': 0.4, 'ADJ': 0.6}
@@ -19,7 +19,7 @@ class tagger_1(EntropyTaggerI):
         return 'ADJ'
 
     def entropy(self, w):
-        return 0.3
+        return 0.5
 
 
 class tagger_2(EntropyTaggerI):
@@ -34,29 +34,27 @@ class tagger_2(EntropyTaggerI):
 
 class EntropyVotingTagger(SequentialBackoffTagger):
 
-    def __init__(self, taggers=[], max_entropy=0.6):
-        self._taggers = taggers
+    def __init__(self, taggers, max_entropy=0.6):
+        super(EntropyVotingTagger, self).__init__(backoff=taggers)
         self.max_entropy = max_entropy
 
-    def choose_tag(self, tokens, index, history):
-        print '!!!!!!!'
+    def tag_one(self, tokens, index, history):
         token = tokens[index]
 
-        best_tagger = min(self._taggers, key=lambda tagger: tagger.entropy(token))
-        import pdb; pdb.set_trace()
-        if best_tagger is None or best_tagger.entropy() > self.max_entropy:
+        #the slicing is because the first tagger is 'self'
+        best_tagger = min(self._taggers[1:], key=lambda tagger: tagger.entropy(token))
+        if best_tagger is None or best_tagger.entropy(token) > self.max_entropy:
             return None
-
-        return best_tagger.choose_tag(token, index, history)
+        return best_tagger.choose_tag(tokens, index, history)
 
 if __name__ == '__main__':
     tagger1 = tagger_1()
-    tagger2 = tagger_2()
+    tagger2 = tagger_2(backoff=tagger1)
 
     # voting_tagger = EntropyVotingTagger([tagger1, tagger2])
     # result = voting_tagger.tag(('hello', 'world', 'this', 'is', 'test'))
     # print result
 
-    voting_tagger = EntropyVotingTagger([tagger1, tagger2], max_entropy=0.1)
+    voting_tagger = EntropyVotingTagger(tagger2, max_entropy=0.8)
     result = voting_tagger.tag(('hello', 'world', 'this', 'is', 'test'))
     print result
