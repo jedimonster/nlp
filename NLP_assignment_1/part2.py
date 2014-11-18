464f9f4335d78c6792c2aea07ad538dc0e560cf8
jedimonster <itay.itay@gmail.com>
2014-11-18 17:33:24 +0200

    finished unigram tagger

--- NLP_assignment_1/part2.py
+++ NLP_assignment_1/part2.py
@@ -8,29 +8,33 @@ __author__ = 'itay'
 class SimpleUnigramTagger(TaggerI):
     def __init__(self, train=None, model=None,
                  backoff=None, cutoff=0, verbose=False):
-        self.freq = ConditionalFreqDist(train)
+        # list comprehension here just flattens the sentences.
+        combined_sents = (item for sublist in train for item in sublist)
+        self.freq = ConditionalFreqDist(combined_sents)
 
     def tag(self, tokens):
-        [self.tag_word(word) for word in tokens]
+        return [self.tag_word(word) for word in tokens]
 
     def tag_word(self, word):
         if word not in self.freq:
-            return None
+            return word, None
         m = max(self.freq[word].items(), key=lambda x: x[1])
-        return m[0]
+        return word, m[0]
 
 
-# split the brown corpus to test, dev, and test set
-all_words = corpus.brown.tagged_words(tagset='universal')
-# random.shuffle(all_words)  # we shuffle it so we don't get a specific category as the test set!
+if __name__ == "__main__":
+    # split the brown corpus to test, dev, and test set
+    all_words = corpus.brown.tagged_sents(tagset='universal', categories='news')
+    # random.shuffle(all_words)  # we shuffle it so we don't get a specific category as the test set!
+    ds_length = len(all_words)
+    train = all_words[:int(0.1 * ds_length)]
+    dev = all_words[int(0.1 * ds_length):int(0.2 * ds_length)]
+    test = all_words[int(0.2) * ds_length:]
 
-ds_length = len(all_words)
-train = all_words[:int(0.1 * ds_length)]
-dev = all_words[int(0.1 * ds_length):int(0.2 * ds_length)]
-test = all_words[int(0.2) * ds_length:]
+    our_tagger = SimpleUnigramTagger(train=train)
+    # import pdb;pdb.set_trace()
 
-our_tagger = SimpleUnigramTagger(train=train)
-print our_tagger.evaluate(test)
+    print our_tagger.evaluate(test)
 
-their_tagger = NgramTagger(1, train=train)
-print their_tagger.evaluate(test)
+    their_tagger = NgramTagger(1, train=train)
+    print their_tagger.evaluate(test)
